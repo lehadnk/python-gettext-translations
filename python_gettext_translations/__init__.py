0a3e59e60a14ea0116b88688a7ccb179ebc02452
Alexey Zauzin <lehadnk@gmail.com>
2024-03-21 01:12:46 +0000

    Fixed requirements

--- python_gettext_translations/__init__.py
+++ python_gettext_translations/__init__.py
@@ -1,5 +1,5 @@
-__version_info__ = (0, 0, 2)
-__version__ = "0.0.2"
+__version_info__ = (0, 0, 3)
+__version__ = "0.0.3"
 
 def setup(app):
     return {"version": __version__, "parallel_read_safe": True}
